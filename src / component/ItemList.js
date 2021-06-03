81f8cb30260075716b1ea5eaeaad992d40222b89
이종원 <ijong-won@ijong-won-ui-MacBookPro.local>
2021-06-04 03:21:49 +0900

    used Next/Link

--- src / component/ItemList.js
+++ src / component/ItemList.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styles from "./ItemList.module.css";
+import Link from "next/link";
 
 function ItemList({ list }) {
   return (
@@ -6,13 +8,23 @@ function ItemList({ list }) {
       <div class="ui three column divided grid">
         <div class="row">
           {list.map((item) => (
-            <div class="column">
-              <img src={item.image_link} alt={item.name} />
-              <strong>{item.name}</strong>
-              <span>
-                {item.category} {item.product_type}
-              </span>
-              <strong>${item.price}</strong>
+            <div class="column" key={item.id}>
+              <Link href={`/view/${item.id}`}>
+                <a>
+                  <div className={styles.wrap}>
+                    <img
+                      src={item.image_link}
+                      alt={item.name}
+                      className={styles.img_item}
+                    />
+                    <strong className={styles.tit_item}>{item.name}</strong>
+                    <span className={styles.txt_info}>
+                      {item.category} {item.product_type}
+                    </span>
+                    <strong className={styles.num_price}>${item.price}</strong>
+                  </div>
+                </a>
+              </Link>
             </div>
           ))}
         </div>
